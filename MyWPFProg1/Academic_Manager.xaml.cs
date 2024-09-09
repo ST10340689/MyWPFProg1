5445201a808af47d8df1818f51f39a45b4b4e6f1
RC_Student_lab <RC_Student_lab@labG9AEB3>
2024-09-09 14:08:15 +0000

    Add project files.

---
+++ MyWPFProg1/Academic_Manager.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MyWPFProg1
+{
+    /// <summary>
+    /// Interaction logic for Academic_Manager.xaml
+    /// </summary>
+    public partial class Academic_Manager : Window
+    {
+        public Academic_Manager()
+        {
+            InitializeComponent();
+        }
+    }
+}
